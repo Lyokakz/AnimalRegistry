dc03bde270cdf96261db912eb9c053e274e2abff
Lyokakz <lenusik__rybka@mail.ru>
2025-04-26 13:51:20 +0500

    Архив с выполненным заданием по MySQL.

---
+++ HumanFriends_backup.sql
@@ -0,0 +1,344 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Linux (x86_64)
+--
+-- Host: localhost    Database: HumanFriends
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `animal_commands`
+--
+
+DROP TABLE IF EXISTS `animal_commands`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `animal_commands` (
+  `animal_id` int DEFAULT NULL,
+  `id_kind` int DEFAULT NULL,
+  `id_commands` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `animal_commands`
+--
+
+LOCK TABLES `animal_commands` WRITE;
+/*!40000 ALTER TABLE `animal_commands` DISABLE KEYS */;
+INSERT INTO `animal_commands` VALUES (1,1,1),(1,1,2),(1,1,3),(2,1,1),(2,1,7),(2,1,8),(3,1,1),(3,1,2),(3,1,5),(1,2,1),(1,2,4),(2,2,1),(2,2,4),(2,2,9),(3,2,11),(3,2,9),(3,2,12),(1,3,5),(1,3,6),(2,3,5),(2,3,10),(1,4,13),(1,4,14),(1,4,15),(2,4,13),(2,4,14),(3,4,13),(3,4,12),(3,4,15),(1,5,16),(1,5,17),(2,5,16),(2,5,1),(3,5,16),(3,5,20),(1,6,16),(1,6,17),(1,6,18),(2,6,16),(2,6,18),(2,6,19);
+/*!40000 ALTER TABLE `animal_commands` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `animal_types`
+--
+
+DROP TABLE IF EXISTS `animal_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `animal_types` (
+  `id_type` int NOT NULL AUTO_INCREMENT,
+  `type_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_type`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `animal_types`
+--
+
+LOCK TABLES `animal_types` WRITE;
+/*!40000 ALTER TABLE `animal_types` DISABLE KEYS */;
+INSERT INTO `animal_types` VALUES (1,'Pets'),(2,'PackAnimals');
+/*!40000 ALTER TABLE `animal_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `animals`
+--
+
+DROP TABLE IF EXISTS `animals`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `animals` (
+  `animal_id` int DEFAULT NULL,
+  `id_kind` int DEFAULT NULL,
+  `id_type` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `animals`
+--
+
+LOCK TABLES `animals` WRITE;
+/*!40000 ALTER TABLE `animals` DISABLE KEYS */;
+INSERT INTO `animals` VALUES (1,1,1),(2,1,1),(3,1,1),(1,2,1),(2,2,1),(3,2,1),(1,3,1),(2,3,1),(1,4,2),(2,4,2),(3,4,2),(1,5,2),(2,5,2),(3,5,2),(1,6,2),(2,6,2);
+/*!40000 ALTER TABLE `animals` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `camels`
+--
+
+DROP TABLE IF EXISTS `camels`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `camels` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `birth_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `camels`
+--
+
+LOCK TABLES `camels` WRITE;
+/*!40000 ALTER TABLE `camels` DISABLE KEYS */;
+INSERT INTO `camels` VALUES (1,'Sandy','2016-11-03'),(2,'Dune','2018-12-12'),(3,'Sahara','2015-08-14');
+/*!40000 ALTER TABLE `camels` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cats`
+--
+
+DROP TABLE IF EXISTS `cats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cats` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `birth_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cats`
+--
+
+LOCK TABLES `cats` WRITE;
+/*!40000 ALTER TABLE `cats` DISABLE KEYS */;
+INSERT INTO `cats` VALUES (1,'Whiskers','2019-05-15'),(2,'Smudge','2020-02-20'),(3,'Oliver','2020-06-30');
+/*!40000 ALTER TABLE `cats` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `commands`
+--
+
+DROP TABLE IF EXISTS `commands`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `commands` (
+  `id_commands` int NOT NULL AUTO_INCREMENT,
+  `commands_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_commands`),
+  UNIQUE KEY `commands_name` (`commands_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `commands`
+--
+
+LOCK TABLES `commands` WRITE;
+/*!40000 ALTER TABLE `commands` DISABLE KEYS */;
+INSERT INTO `commands` VALUES (8,'Bark'),(18,'Bray'),(14,'Canter'),(17,'Carry Load'),(3,'Fetch'),(15,'Gallop'),(6,'Hide'),(12,'Jump'),(19,'Kick'),(11,'Meow'),(7,'Paw'),(4,'Pounce'),(5,'Roll'),(20,'Run'),(9,'Scratch'),(1,'Sit'),(10,'Spin'),(2,'Stay'),(13,'Trot'),(16,'Walk');
+/*!40000 ALTER TABLE `commands` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dogs`
+--
+
+DROP TABLE IF EXISTS `dogs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dogs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `birth_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dogs`
+--
+
+LOCK TABLES `dogs` WRITE;
+/*!40000 ALTER TABLE `dogs` DISABLE KEYS */;
+INSERT INTO `dogs` VALUES (1,'Fido','2020-01-01'),(2,'Buddy','2018-12-10'),(3,'Bella','2019-11-11');
+/*!40000 ALTER TABLE `dogs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `donkeys`
+--
+
+DROP TABLE IF EXISTS `donkeys`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `donkeys` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `birth_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `donkeys`
+--
+
+LOCK TABLES `donkeys` WRITE;
+/*!40000 ALTER TABLE `donkeys` DISABLE KEYS */;
+INSERT INTO `donkeys` VALUES (1,'Eeyore','2017-09-18'),(2,'Burro','2019-01-23');
+/*!40000 ALTER TABLE `donkeys` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hamsters`
+--
+
+DROP TABLE IF EXISTS `hamsters`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hamsters` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `birth_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hamsters`
+--
+
+LOCK TABLES `hamsters` WRITE;
+/*!40000 ALTER TABLE `hamsters` DISABLE KEYS */;
+INSERT INTO `hamsters` VALUES (1,'Hammy','2021-03-10'),(2,'Peanut','2021-08-01');
+/*!40000 ALTER TABLE `hamsters` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `horses`
+--
+
+DROP TABLE IF EXISTS `horses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `horses` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `birth_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `horses`
+--
+
+LOCK TABLES `horses` WRITE;
+/*!40000 ALTER TABLE `horses` DISABLE KEYS */;
+INSERT INTO `horses` VALUES (1,'Thunder','2015-07-21'),(2,'Storm','2014-05-05'),(3,'Blaze','2016-02-29');
+/*!40000 ALTER TABLE `horses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `kind`
+--
+
+DROP TABLE IF EXISTS `kind`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `kind` (
+  `id_kind` int NOT NULL AUTO_INCREMENT,
+  `kind_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_kind`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `kind`
+--
+
+LOCK TABLES `kind` WRITE;
+/*!40000 ALTER TABLE `kind` DISABLE KEYS */;
+INSERT INTO `kind` VALUES (1,'Dog'),(2,'Cat'),(3,'Hamster'),(4,'Horse'),(5,'Camel'),(6,'Donkey');
+/*!40000 ALTER TABLE `kind` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pack_animals`
+--
+
+DROP TABLE IF EXISTS `pack_animals`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pack_animals` (
+  `animal_id` int DEFAULT NULL,
+  `id_kind` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pack_animals`
+--
+
+LOCK TABLES `pack_animals` WRITE;
+/*!40000 ALTER TABLE `pack_animals` DISABLE KEYS */;
+INSERT INTO `pack_animals` VALUES (1,4),(2,4),(3,4),(1,5),(2,5),(3,5),(1,6),(2,6);
+/*!40000 ALTER TABLE `pack_animals` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pets`
+--
+
+DROP TABLE IF EXISTS `pets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pets` (
+  `animal_id` int DEFAULT NULL,
+  `id_kind` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pets`
+--
+
+LOCK TABLES `pets` WRITE;
+/*!40000 ALTER TABLE `pets` DISABLE KEYS */;
+INSERT INTO `pets` VALUES (1,1),(2,1),(3,1),(1,2),(2,2),(3,2),(1,3),(2,3);
+/*!40000 ALTER TABLE `pets` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-26 13:47:25
